d3cc52c90a6f041e2d07d7fffc71ad1ee8972e59
Youkoulayley <bmayelle@hotmail.fr>
2016-08-17 23:05:59 +0200

    Add Breaking Bad Maintenance

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -8,6 +8,11 @@
         <style>
             html, body {
                 height: 100%;
+                background: url(images/breaking_bad_maintenance.jpg) no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
             }
 
             body {
