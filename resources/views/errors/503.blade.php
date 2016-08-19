d4aeedde91d4738b98805c512d6043557fa7f5ae
Youkoulayley <bmayelle@hotmail.fr>
2016-08-19 10:41:55 +0200

    Change color maintenance

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -19,7 +19,7 @@
                 margin: 0;
                 padding: 0;
                 width: 100%;
-                color: white;
+                color: black;
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato';
