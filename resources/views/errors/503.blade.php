fbf7bb9c69a6b36a85a936b77d11a6666ad9f6ae
Youkoulayley <bmayelle@hotmail.fr>
2016-08-19 10:48:05 +0200

    Remove Vertical-Align

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -28,7 +28,6 @@
             .container {
                 text-align: center;
                 display: table-cell;
-                vertical-align: middle;
             }
 
             .content {
