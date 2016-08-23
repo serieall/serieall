851acdad57e90968c4af11919e6c65a2b03b0386
Youkoulayley <bmayelle@hotmail.fr>
2016-08-23 11:31:28 +0200

    Oubli de mot de passe

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Le mot de passe doit faire au moins 6 caractères et correspondre avec la confirmation.',
+    'reset' => 'Votre mot de passe a été mis à jour !',
+    'sent' => 'Nous vous avons envoyé le lien de réinitialisation du mot de passe!',
+    'token' => 'Votre clé de réinitialisation est invalide.',
+    'user' => "Nous ne trouvons pas d'utilisateur avec cette adresse e-mail",
 
 ];
--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,10 +1,10 @@
-Bonjour,
-
-Vous avez effectué une demande de changement de mot de passe suite à un oubli.
-
-
-Cliquez sur le lien ci dessous pour réinitialiser votre mot de passe :
-<a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
-
-A bientôt !
+Bonjour,<br />
+<br />
+Vous avez effectué une demande de changement de mot de passe suite à un oubli.<br />
+<br />
+<br />
+Cliquez sur le lien ci dessous pour réinitialiser votre mot de passe :<br />
+<a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a><br/>
+<br/>
+A bientôt !<br/>
 L'équipe Série-All
