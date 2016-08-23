a3bdbdd8cd3a523f5ee4134a4827d490e516da7d
Youkoulayley <bmayelle@hotmail.fr>
2016-08-23 15:31:07 +0200

    login -> username

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('login')->unique();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->tinyInteger('role')->default(1);
