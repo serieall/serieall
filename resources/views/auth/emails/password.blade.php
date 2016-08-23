5dcf99ce3032bfdb75acaa541fddaa02745cf011
Youkoulayley <bmayelle@hotmail.fr>
2016-08-23 11:24:57 +0200

    Oubli de mot de passe

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,10 @@
-Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+Bonjour,
+
+Vous avez effectué une demande de changement de mot de passe suite à un oubli.
+
+
+Cliquez sur le lien ci dessous pour réinitialiser votre mot de passe :
+<a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+
+A bientôt !
+L'équipe Série-All
