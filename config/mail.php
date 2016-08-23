0758235131de396788d63eae0e51e3fc3e890804
Youkoulayley <bmayelle@hotmail.fr>
2016-08-23 10:41:05 +0200

    Configuration mail

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'journeytotheit@gmail.com', 'name' => 'SérieAll'],
 
     /*
     |--------------------------------------------------------------------------
