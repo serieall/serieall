c7b48999780c823e4b5cadc0bf40679a454fe003
Youkoulayley <bmayelle@hotmail.fr>
2016-08-24 16:40:13 +0200

    add username to profil

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Profil de <?php Auth::user()->username; ?></div>
+                    <div class="panel-heading">Profil de {{ Auth::user()->username }}</div>
                 </div>
             </div>
         </div>
