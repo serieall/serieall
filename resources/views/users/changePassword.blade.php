0740cd6f70f94a88d8b44bcee0d07a3ad5915505
Youkoulayley <bmayelle@hotmail.fr>
2016-08-25 16:50:43 +0200

    Center bouton submit

--- resources/views/users/changePassword.blade.php
+++ resources/views/users/changePassword.blade.php
@@ -74,7 +74,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-key"></i> S'inscrire
+                                    <i class="fa fa-btn fa-key"></i> Changer le mot de passe
                                 </button>
                             </div>
                         </div>
