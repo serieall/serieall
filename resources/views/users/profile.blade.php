1d1d0c1a49346e318bc4dc74f4694b4a527a4074
Youkoulayley <bmayelle@hotmail.fr>
2016-08-25 11:54:32 +0200

    add page profile

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -6,21 +6,21 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Profil de {{ $user->username }}</div>
-                </div>
-                @if (Auth::guest())
-                    <div class="panel-body">
-                        Mais tu n'es pas {{ $user->username }} !
-                    </div>
-                @elseif(Auth::user()->id == $user->id)
-                    <div class="panel-body">
-                        C'est bien ton compte. T'es balèze.
-                    </div>
-                @else
-                    <div class="panel-body">
-                        Mais tu n'es pas {{ $user->username }} ! Mais au moins t'es connecté, c'est bien.
-                    </div>
-                @endif
 
+                    @if (Auth::guest())
+                        <div class="panel-body">
+                            Mais tu n'es pas {{ $user->username }} !
+                        </div>
+                    @elseif(Auth::user()->id == $user->id)
+                        <div class="panel-body">
+                            C'est bien ton compte. T'es balèze.
+                        </div>
+                    @else
+                        <div class="panel-body">
+                            Mais tu n'es pas {{ $user->username }} ! Mais au moins t'es connecté, c'est bien.
+                        </div>
+                    @endif
+                </div>
             </div>
         </div>
     </div>
