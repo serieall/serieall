61552523637adc2507ad2659d248fa8e742345bc
Youkoulayley <bmayelle@hotmail.fr>
2016-09-01 11:21:21 +0200

    Changement de mot de passe en Javascript

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -4,6 +4,11 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
+                <div class="alert alert-success alert-dismissible hidden">
+
+                    Ton mot de passe a bien été changé.
+
+                </div>
                 <div class="panel panel-default">
                     <div class="panel-heading">Profil de {{ $user->username }}</div>
 
@@ -16,10 +21,101 @@
                     @elseif(Auth::user()->id == $user->id)
                         <div class="panel-body">
                             Pour changer de mot de passe, clique sur le bouton ci-dessous. Ton ancien mot de passe te sera demandé.
-                            <form class="center form-horizontal" role="form" method="GET" action="{{ url('profil/' . Auth::user()->username . '/changepassword') }}">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-key"></i> Changer mon mot de passe
-                                </button>
+                            <a class="btn btn-link" id="changepassword" href="#">Changer mon mot de passe</a>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                                                <h4 class="modal-title" id="myModalLabel">Changer mon mot de passe</h4>
+                                            </div>
+                                            <div class="modal-body">
+
+                                                <form id="formChangePwd" class="form-horizontal" role="form" method="POST" action="{{ url('/profil', Auth::user()->username)) }}">
+                                                    {!! csrf_field() !!}
+
+                                                    <div class="form-group">
+                                                        <label class="col-md-4 control-label">Ancien mot de passe</label>
+                                                        <div class="col-md-6">
+                                                            <input type="password" class="form-control" name="password_old">
+                                                            <small class="help-block"></small>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <label class="col-md-4 control-label">Nouveau mot de passe</label>
+                                                        <div class="col-md-6">
+                                                            <input type="password" class="form-control" name="password">
+                                                            <small class="help-block"></small>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <label class="col-md-4 control-label">Confirmation du nouveau mot de passe</label>
+                                                        <div class="col-md-6">
+                                                            <input type="password" class="form-control" name="password_confirmation">
+                                                            <small class="help-block"></small>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <div class="col-md-6 col-md-offset-4">
+                                                            <button type="submit" class="btn btn-primary">
+                                                                Confirmer
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                @endsection
+
+                                @section('scripts')
+
+                                    <script>
+
+                                        $(function(){
+
+                                            $('#changepassword').click(function() {
+                                                $('#myModal').modal();
+                                            });
+
+                                            $(document).on('submit', '#formChangePwd', function(e) {
+                                                e.preventDefault();
+
+                                                $('input+small').text('');
+                                                $('input').parent().removeClass('has-error');
+
+                                                $.ajax({
+                                                    method: $(this).attr('method'),
+                                                    url: $(this).attr('action'),
+                                                    data: $(this).serialize(),
+                                                    dataType: "json"
+                                                })
+                                                        .done(function(data) {
+                                                            $('.alert-success').removeClass('hidden');
+                                                            $('#myModal').modal('hide');
+                                                        })
+                                                        .fail(function(data) {
+                                                            $.each(data.responseJSON, function (key, value) {
+                                                                var input = '#formChangePwd input[name=' + key + ']';
+                                                                $(input + '+small').text(value);
+                                                                $(input).parent().addClass('has-error');
+                                                            });
+                                                        });
+                                            });
+
+                                        })
+
+                                    </script>
+
+                            @endsection
                         </div>
                     <!-- Si je suis connecté mais que je ne suis pas l'utilisateur -->
                     @else
