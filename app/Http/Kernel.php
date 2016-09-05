1fc2c6163eb867946ae6e8fec136be42c808c770
Youkoulayley <bmayelle@hotmail.fr>
2016-09-05 10:19:59 +0200

    Changement de mot de passe en Javascript

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,5 +50,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\Admin::class,
+        'ajax' => \App\Http\Middleware\Ajax::class,
     ];
 }
