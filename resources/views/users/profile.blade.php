9f0e82e3006c58a3d9a9c2fb472ec12db3d72f89
Youkoulayley <bmayelle@hotmail.fr>
2016-09-05 10:55:57 +0200

    Changement de mot de passe en Javascript

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -24,6 +24,64 @@
                             <a class="btn btn-link" id="ChangePwd" href="#">Changer mon mot de passe</a>
                         </div>
 
+                        <!-- Modal -->
+                        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                                        <h4 class="modal-title" id="myModalLabel">Register</h4>
+                                    </div>
+                                    <div class="modal-body">
+
+                                        <form id="formRegister" class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                                            {!! csrf_field() !!}
+
+                                            <div class="form-group">
+                                                <label class="col-md-4 control-label">Name</label>
+                                                <div class="col-md-6">
+                                                    <input type="text" class="form-control" name="name">
+                                                    <small class="help-block"></small>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-md-4 control-label">E-Mail Address</label>
+                                                <div class="col-md-6">
+                                                    <input type="email" class="form-control" name="email">
+                                                    <small class="help-block"></small>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-md-4 control-label">Password</label>
+                                                <div class="col-md-6">
+                                                    <input type="password" class="form-control" name="password">
+                                                    <small class="help-block"></small>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label class="col-md-4 control-label">Confirm Password</label>
+                                                <div class="col-md-6">
+                                                    <input type="password" class="form-control" name="password_confirmation">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <div class="col-md-6 col-md-offset-4">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        Register
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     <!-- Si je suis connecté mais que je ne suis pas l'utilisateur -->
                     @else
                         <div class="panel-body">
@@ -35,4 +93,45 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+
+    <script>
+
+        $(function(){
+
+            $('#register').click(function() {
+                $('#myModal').modal();
+            });
+
+            $(document).on('submit', '#formRegister', function(e) {
+                e.preventDefault();
+
+                $('input+small').text('');
+                $('input').parent().removeClass('has-error');
+
+                $.ajax({
+                    method: $(this).attr('method'),
+                    url: $(this).attr('action'),
+                    data: $(this).serialize(),
+                    dataType: "json"
+                })
+                        .done(function(data) {
+                            $('.alert-success').removeClass('hidden');
+                            $('#myModal').modal('hide');
+                        })
+                        .fail(function(data) {
+                            $.each(data.responseJSON, function (key, value) {
+                                var input = '#formRegister input[name=' + key + ']';
+                                $(input + '+small').text(value);
+                                $(input).parent().addClass('has-error');
+                            });
+                        });
+            });
+
+        })
+
+    </script>
+
 @endsection
