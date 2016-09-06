c1c50b3f6cb01952ee30f85999d708bbdf17ea4e
Youkoulayley <bmayelle@hotmail.fr>
2016-09-06 22:23:13 +0200

    Ajout de la partie administration

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div id="content-index-admin" class="flex-container">
-        <div class=".flex-item-center">
+        <div class="flex-item-center">
             <h1>Bienvenue sur l'interface d'administration de Série-All.</h1>
             <p>Veuillez choisir une section dans la partie de gauche.</p>
         </div>
