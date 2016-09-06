61354abca617bf301f73001a1fa4d6c42ada42f2
Youkoulayley <bmayelle@hotmail.fr>
2016-09-06 14:33:13 +0200

    Ajout de la partie administration

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,10 +19,10 @@
     <nav id="nav-admin" class="fl w10">
         <img id="logo-admin" src="images/logo_v2.png" alt="Logo Série-All" />
         <ul id="nav-ul-admin" class="w100">
-            <li id="nav-li-admin" class="pam w100"><a href="#" id="nav-a-admin">Séries<span class="fa fa-chevron-right fa-2x fr"></span></a></li>
-            <li id="nav-li-admin" class="pam w100"><a href="#" id="nav-a-admin">Articles<span class="fa fa-chevron-right fa-2x fr"></span></a></li>
-            <li id="nav-li-admin" class="pam w100"><a href="#" id="nav-a-admin">Utilisateurs<span class="fa fa-chevron-right fa-2x fr"></span></a></li>
-            <li id="nav-li-admin" class="pam w100"><a href="#" id="nav-a-admin">Système <span class="fa fa-chevron-right fa-2x fr"></span></a></li>
+            <li id="nav-li-admin" class="pam w100"><a href="#" id="nav-a-admin">Séries<span id="nav-icon-admin" class="fa fa-chevron-right fr"></span></a></li>
+            <li id="nav-li-admin" class="pam w100"><a href="#" id="nav-a-admin">Articles<span id="nav-icon-admin" class="fa fa-chevron-right fr"></span></a></li>
+            <li id="nav-li-admin" class="pam w100"><a href="#" id="nav-a-admin">Utilisateurs<span id="nav-icon-admin" class="fa fa-chevron-right fr"></span></a></li>
+            <li id="nav-li-admin" class="pam w100"><a href="#" id="nav-a-admin">Système <span id="nav-icon-admin" class="fa fa-chevron-right fr"></span></a></li>
         </ul>
     </nav>
 
