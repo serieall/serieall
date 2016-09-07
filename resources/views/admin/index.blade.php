ed201da1ad1fbb4cacb3fab026d8f5642dbad4df
Youkoulayley <bmayelle@hotmail.fr>
2016-09-07 13:01:12 +0200

    Ajout de la partie administration

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,16 +2,20 @@
 @section('header')
     <header id="header-admin" class="fr w90 pas">
         <div id="header-grid-admin" class="grid-2-1">
+            <div>
             <ul id="header-beadcrumbs-admin" class="fl">
                 <li class="active">
                     Administration
                 </li>
             </ul>
+            </div>
+            <div>
             <ul id="header-user-admin" class="txtright fr">
                 <li>
                     {{ Auth::user()->username }}
                 </li>
             </ul>
+            </div>
         </div>
     </header>
 @endsection
