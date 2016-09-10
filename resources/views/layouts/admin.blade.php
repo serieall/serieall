5eb00d5008bcca619b3cdaa5aec683414d9db8a3
Youkoulayley <bmayelle@hotmail.fr>
2016-09-10 12:13:21 +0200

    Ajout de la partie administration

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,9 +25,9 @@
                 <li>
                     <div id="header-dropdown-admin">
                         <span id="header-dropdown-button">{{ Auth::user()->username }}</span> <span class="caret"></span>
-                        <div id="header-dropdown-content" class="txtcenter">
-                            <a class= "pas" href="{{ url('/')}}">Retour sur le site</a>
-                            <a class= "pas" href="{{ url('/logout')}}">Déconnexion</a>
+                        <div id="header-dropdown-content" class="txtleft">
+                            <a class= "fa fa-caret-square-o-left" href="{{ url('/')}}">Retour sur le site</a>
+                            <a class= "fa fa-sign-out" href="{{ url('/logout')}}">Déconnexion</a>
                         </div>
                     </div>
                 </li>
