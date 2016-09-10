c4291cd1e20f287b845305d9dcfa15eee28db742
Youkoulayley <bmayelle@hotmail.fr>
2016-09-10 14:13:45 +0200

    Ajout de la partie administration

--- config/app.php
+++ config/app.php
@@ -207,7 +207,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Html' => Illuminate\Html\HtmlFacadet::class,
+        'Html' => Illuminate\Html\HtmlFacade::class,
 
     ],
 
