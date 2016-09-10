b006db646c4016ebba9be5df28480e80be0423ee
Youkoulayley <bmayelle@hotmail.fr>
2016-09-10 14:07:04 +0200

    Ajout de la partie administration

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Illuminate\Html\HtmlBuilder::class,
 
         /*
          * Application Service Providers...
@@ -206,6 +207,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Html' => Illuminate\Html\HtmlBuilder::class,
 
     ],
 
