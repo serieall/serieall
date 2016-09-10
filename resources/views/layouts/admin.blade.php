96e20b675712501345c99ca250822d9f3f013f1b
Youkoulayley <bmayelle@hotmail.fr>
2016-09-10 13:24:26 +0200

    Ajout de la partie administration

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,7 +20,7 @@
             </ul>
             <ul id="header-user-admin" class="txtright h50p">
                 <li>
-                    Wiki
+                    Wiki <span class="mls fa fa-question-circle"></span>
                 </li>
                 <li>
                     <div id="header-dropdown-admin">
