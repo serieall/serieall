ee1830c6b3ac0f1607fa1f2c71c22b586167a275
Youkoulayley <bmayelle@hotmail.fr>
2016-09-13 15:58:17 +0200

    Create show table

--- database/seeds/ShowsTableSeeder.php
+++ database/seeds/ShowsTableSeeder.php
@@ -11,18 +11,32 @@ class ShowsTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i = 1; $i <= 100; ++$i)
+        DB::table('posts')->delete();
+
+        for($i = 0; $i < 100; ++$i)
         {
-            $numbers = range(1, 20);
-            shuffle($numbers);
-            $n = rand(3, 6);
-            for($j = 1; $j < $n; ++$j)
-            {
-                DB::table('nationality_show')->insert(array(
-                    'show_id' => $i,
-                    'nationality_id' => $numbers[$j]
-                ));
-            }
+            $date = $this->randDate();
+            DB::table('posts')->insert([
+                'thetvdbid' => rand(1, 10),
+                'show_url' => 'titre' . $i,
+                'name' => 'Titre' . $i,
+                'name_fr' => 'TitreFR' . $i,
+                'synopsis' => 'Contenu' . $i . ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                'format' => 45,
+                'annee' => rand(2010, 2016),
+                'encours' => rand (0,1),
+                'createurs' => 'Createurs' . $i,
+                'diffusion_us' => $date,
+                'diffusion_fr' => $date,
+                'moyenne' => rand(0,20),
+                'moyenne_redac' => rand(0,20),
+                'nbnotes' => rand(0,50),
+                'taux_erectile' => rand(0,100),
+                'avis_rentree' => 'Avis' . $i,
+                'created_at' => $date,
+                'updated_at' => $date
+            ]);
+
         }
     }
 }
