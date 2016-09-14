75d11df0aef97adb78355bef7165b88b82089aef
Youkoulayley <bmayelle@hotmail.fr>
2016-09-14 23:26:15 +0200

    Ajout de la partie administration

--- app/Repositories/AdminRepository.php
+++ app/Repositories/AdminRepository.php
@@ -15,7 +15,8 @@ class AdminRepository
     }
 
     public function getShowByName($n){
-        return $this->show->with('channels', 'nationalities', 'seasons')
+        return $this->show->with('channels', 'nationalities')
+            ->withCount('seasons')
             ->orderBy('shows.name')
             ->paginate($n);
     }
