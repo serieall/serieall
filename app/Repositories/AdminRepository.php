c7a4cb8cd6dff4095365436ced0da56f3be7fdd5
Youkoulayley <bmayelle@hotmail.fr>
2016-09-15 16:21:28 +0200

    delete box shadow in admin layout

--- app/Repositories/AdminRepository.php
+++ app/Repositories/AdminRepository.php
@@ -18,7 +18,7 @@ class AdminRepository
     public function getShowByName($n){
 
         $shows=DB::table('shows')
-            ->select('shows.name', 'count(req.season)', 'sum(req.episodes)')
+            ->select('shows.name', DB::raw('count(req.season) as NB_SAISONS, sum(req.episodes AS NB_EPISODES)'))
             ->join(DB::raw('(SELECT seasons.id season, 
                         seasons.show_id, 
                         COUNT(episodes.id) episodes
