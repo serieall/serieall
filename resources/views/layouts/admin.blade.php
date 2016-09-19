9db454400ff015e88074e04609b7bef9e4dc52d8
Youkoulayley <bmayelle@hotmail.fr>
2016-09-19 22:57:13 +0200

    dropdown change

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,10 +9,9 @@
     <link rel="icon" href="images/logo_v2.ico">
 
     <!-- CSS -->
-    {{ Html::style('/semantic/semantic.css') }}
+    {{ Html::style('/css/knacss.css') }}
     {{ Html::style('/css/knacss_perso.css') }}
     {{ Html::style('/css/font-awesome.css') }}
-    <script src="/semantic/semantic.min.js"></script>
 
     <!-- Javascript -->
     {{ Html::script('/js/jquery.js') }}
