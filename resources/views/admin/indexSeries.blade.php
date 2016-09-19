471bc85bcdf29b7448b158f2bec3a9b969e72c9f
Youkoulayley <bmayelle@hotmail.fr>
2016-09-19 23:04:20 +0200

    Grid system, biatch

--- resources/views/admin/indexSeries.blade.php
+++ resources/views/admin/indexSeries.blade.php
@@ -16,47 +16,49 @@
         <h1 id="content-h1-admin" class="txtcenter">Liste des séries</h1>
 
         <div class="grid-2">
-            <table id="table-show-admin">
-                <thead>
+            <div class="grid-item-double">
+                <table id="table-show-admin">
+                    <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Chaines</th>
+                            <th>Nationalités</th>
+                            <th>Nombre de saisons</th>
+                            <th>Nombre d'épisodes</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                @foreach($shows as $show)
                     <tr>
-                        <th>Nom</th>
-                        <th>Chaines</th>
-                        <th>Nationalités</th>
-                        <th>Nombre de saisons</th>
-                        <th>Nombre d'épisodes</th>
-                        <th>Actions</th>
+                        <td>
+                            {{ $show->name }}
+                        </td>
+                        <td>
+                            @foreach($show->channels as $channel)
+                                {{ $channel->name }}
+                                <br />
+                            @endforeach
+                        </td>
+                        <td>
+                            @foreach($show->nationalities as $nationality)
+                                {{ $nationality->name }}
+                                <br />
+                            @endforeach
+                        </td>
+                        <td class="txtcenter">
+                            {{ $show->seasons_count }}
+                        </td>
+                        <td class="txtcenter">
+                            {{ $show->episodes_count }}
+                        </td>
+                        <td class="actions txtcenter">
+                            <a href="#"><i class="fa fa-pencil fa-2x"></i></a>
+                            <a href="#"><i class="fa fa-trash fa-2x"></i></a>
+                        </td>
                     </tr>
-                </thead>
-            @foreach($shows as $show)
-                <tr>
-                    <td>
-                        {{ $show->name }}
-                    </td>
-                    <td>
-                        @foreach($show->channels as $channel)
-                            {{ $channel->name }}
-                            <br />
-                        @endforeach
-                    </td>
-                    <td>
-                        @foreach($show->nationalities as $nationality)
-                            {{ $nationality->name }}
-                            <br />
-                        @endforeach
-                    </td>
-                    <td class="txtcenter">
-                        {{ $show->seasons_count }}
-                    </td>
-                    <td class="txtcenter">
-                        {{ $show->episodes_count }}
-                    </td>
-                    <td class="actions txtcenter">
-                        <a href="#"><i class="fa fa-pencil fa-2x"></i></a>
-                        <a href="#"><i class="fa fa-trash fa-2x"></i></a>
-                    </td>
-                </tr>
-            @endforeach
-            </table>
+                @endforeach
+                </table>
+            </div>
         </div>
     </div>
 @endsection
