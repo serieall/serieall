9e5b76939a22e8114710b1bcf1938020909b7cce
Youkoulayley <bmayelle@hotmail.fr>
2016-09-20 18:46:37 +0200

    Changement du design du tableau

--- resources/views/admin/indexSeries.blade.php
+++ resources/views/admin/indexSeries.blade.php
@@ -54,8 +54,8 @@
                         {{ $show->episodes_count }}
                     </td>
                     <td class="center aligned">
-                        <a id="action" href="#"><i class="big circular edit icon"></i></a>
-                        <a id="action" href="#"><i class="big circular trash icon"></i></a>
+                        <a id="action" href="#"><i class="big inverted circular edit icon"></i></a>
+                        <a id="action" href="#"><i class="big inverted circular trash icon"></i></a>
                     </td>
                 </tr>
             @endforeach
