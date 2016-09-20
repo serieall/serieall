51ebc9903e5cda8f54bdbb7b2b48f82c11a6f271
Youkoulayley <bmayelle@hotmail.fr>
2016-09-20 13:52:40 +0200

    Add semantic ui

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -66,7 +66,6 @@
 
         $('.ui.sidebar')
                 .sidebar({
-                    context: $('.bottom.segment'),
                     dimPage: false,
                     closable: false
                 })
