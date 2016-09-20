8b06f00dc6dcd1de5df3fcc71d00eefed9691935
Youkoulayley <bmayelle@hotmail.fr>
2016-09-20 21:56:23 +0200

    Dropdown

--- resources/views/admin/indexSeries.blade.php
+++ resources/views/admin/indexSeries.blade.php
@@ -11,14 +11,38 @@
 @endsection
 
 @section('content')
-    <div class="ui right floated green animated fade button" tabindex="0">
+    <div class="ui right floated green animated fade button" tabindex="0" id="add-serie">
         <div class="visible content">Ajouter une nouvelle série</div>
         <div class="hidden content">
             <i class="plus icon"></i>
         </div>
     </div>
 
-    @extends('serie_add')
+    <div class="ui modal">
+        <i class="close icon"></i>
+        <div class="header">
+            Profile Picture
+        </div>
+        <div class="image content">
+            <div class="ui medium image">
+                <img src="/images/avatar/large/chris.jpg">
+            </div>
+            <div class="description">
+                <div class="ui header">We've auto-chosen a profile image for you.</div>
+                <p>We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank">gravatar</a> image associated with your registered e-mail address.</p>
+                <p>Is it okay to use this photo?</p>
+            </div>
+        </div>
+        <div class="actions">
+            <div class="ui black deny button">
+                Nope
+            </div>
+            <div class="ui positive right labeled icon button">
+                Yep, that's me
+                <i class="checkmark icon"></i>
+            </div>
+        </div>
+    </div>
 
     <div>
         <h1 class="ui header" id="admin-titre">
@@ -100,7 +124,7 @@
             }
         }} );
 
-        $('.ui.modal')
+        $('#add-serie')
         .modal('show')
         ;
 @endsection
