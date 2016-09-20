b2066bcd7d9e31457b84e10dd3ba4cb1f0c5b122
Youkoulayley <bmayelle@hotmail.fr>
2016-09-20 12:39:16 +0200

    Add semantic ui

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -55,26 +55,21 @@
     </header>
     <div class="ui top attached demo menu">
         <a class="item">
-            <i class="sidebar icon"></i>
             Menu
         </a>
     </div>
     <div class="ui bottom attached segment pushable">
         <div style="" class="ui inverted labeled icon left inline vertical sidebar menu uncover visible">
             <a class="item">
-                <i class="home icon"></i>
                 Home
             </a>
             <a class="item">
-                <i class="block layout icon"></i>
                 Topics
             </a>
             <a class="item">
-                <i class="smile icon"></i>
                 Friends
             </a>
             <a class="item">
-                <i class="calendar icon"></i>
                 History
             </a>
         </div>
