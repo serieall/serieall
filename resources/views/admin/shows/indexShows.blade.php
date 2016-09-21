a02f3601686151495ed89bc258ccb38f79395803
Youkoulayley <bmayelle@hotmail.fr>
2016-09-21 09:26:04 +0200

    Ajout du fichier de langue datatables

--- resources/views/admin/shows/indexShows.blade.php
+++ resources/views/admin/shows/indexShows.blade.php
@@ -84,7 +84,17 @@
         $('#table-show-admin').DataTable( {
             "order": [[ 0, "asc" ]],
             "language": {
-                "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json"
+                "lengthMenu": "Afficher _MENU_ enregistrements par page",
+                "zeroRecords": "Aucun enregistrement trouvé",
+                "info": "Page _PAGE_ sur _PAGES_",
+                "infoEmpty": "Aucun enregistrement trouvé",
+                "infoFiltered": "(filtré sur _MAX_ enregistrements)",
+                "sSearch" : "",
+                "oPaginate": {
+                    "sFirst":    	"Début",
+                    "sPrevious": 	"Précédent",
+                    "sNext":     	"Suivant",
+                    "sLast":     	"Fin"
             }
         }} );
 @endsection
