7d5ff1bcf3dbac2d667e06799d72305597b3e1d7
Youkoulayley <bmayelle@hotmail.fr>
2016-09-21 09:19:14 +0200

    Ajout du fichier de langue datatables

--- resources/views/admin/shows/indexShows.blade.php
+++ resources/views/admin/shows/indexShows.blade.php
@@ -84,7 +84,7 @@
         $('#table-show-admin').DataTable( {
             "order": [[ 0, "asc" ]],
             "language": {
-                "url": "//dev.journeytotheit.ovh/language-datatable.json"
+                "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json"
             }
         }} );
 @endsection
