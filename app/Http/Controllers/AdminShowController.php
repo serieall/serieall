7083f8add78770b3fe5def52f7b3fe775db4a96b
Youkoulayley <bmayelle@hotmail.fr>
2016-09-21 14:50:11 +0200

    Passage des URLS aux noms de route

--- app/Http/Controllers/AdminShowController.php
+++ app/Http/Controllers/AdminShowController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Repositories\AdminShowRepository;
 
 use App\Http\Requests;
+use Auth;
 use App\Jobs\SendWelcomeEmail;
 
 class AdminShowController extends Controller
