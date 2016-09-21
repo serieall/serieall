a879dd9f016151febb292d4a72406c8967c64386
Youkoulayley <bmayelle@hotmail.fr>
2016-09-21 08:51:49 +0200

    Ajout du fichier de langue datatables

--- resources/views/admin/shows/addShow.blade.php
+++ resources/views/admin/shows/addShow.blade.php
@@ -1,36 +1,21 @@
-<!DOCTYPE html>
-<html lang="en" id="html-admin">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
 
-    <title>SérieAll BETA</title>
-    <link rel="icon" href="images/logo_v2.ico">
-
-    <!-- CSS -->
-    {{ Html::style('/semantic/semantic.css') }}
-    {{ Html::style('/semantic/semantic_perso.css') }}
-    {{ Html::style('/css/font-awesome.css') }}
-
-    <!-- Javascript -->
-    {{ Html::script('/js/jquery.js') }}
-    {{ Html::script('/js/datatables.js') }}
-    {{ Html::script('/semantic/semantic.js') }}
-
-</head>
-<body>
-<div class="ui vertical inverted sticky menu bound bottom">
-
-    <div class="item">
-        TEST
+@section('breadcrumbs')
+    <a href="{{ url('/admin') }}" class="section">
+        Administration
+    </a>
+    <i class="right angle icon divider"></i>
+    <a href="{{ url('/admin/series') }}" class="section">
+        Séries
+    </a>
+    <i class="right angle icon divider"></i>
+    <div class="active section">
+        Ajouter une série
     </div>
-    <div class="item">
-        TEST
-    </div>
+@endsection
 
-</div>
+@section('content')
 
-BONJOUR
-</body>
-</html>
+
+@endsection
+
--- resources/views/admin/shows/indexShows.blade.php
+++ resources/views/admin/shows/indexShows.blade.php
@@ -84,17 +84,7 @@
         $('#table-show-admin').DataTable( {
             "order": [[ 0, "asc" ]],
             "language": {
-                "lengthMenu": "Afficher _MENU_ enregistrements par page",
-                "zeroRecords": "Aucun enregistrement trouvé",
-                "info": "Page _PAGE_ sur _PAGES_",
-                "infoEmpty": "Aucun enregistrement trouvé",
-                "infoFiltered": "(filtré sur _MAX_ enregistrements)",
-                "sSearch" : "",
-                "oPaginate": {
-                    "sFirst":    	"Début",
-                    "sPrevious": 	"Précédent",
-                    "sNext":     	"Suivant",
-                    "sLast":     	"Fin"
+                "url": "/public/language-datatable.json",
             }
         }} );
 @endsection
