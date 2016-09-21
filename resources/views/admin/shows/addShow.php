055f68fbde7cf59b18a3228786a0515068643109
Youkoulayley <bmayelle@hotmail.fr>
2016-09-21 08:40:28 +0200

    Ajout de la route vers l'ajout d'une série

--- resources/views/admin/shows/addShow.php
+++ resources/views/admin/shows/addShow.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" id="html-admin">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SérieAll BETA</title>
+    <link rel="icon" href="images/logo_v2.ico">
+
+    <!-- CSS -->
+    {{ Html::style('/semantic/semantic.css') }}
+    {{ Html::style('/semantic/semantic_perso.css') }}
+    {{ Html::style('/css/font-awesome.css') }}
+
+    <!-- Javascript -->
+    {{ Html::script('/js/jquery.js') }}
+    {{ Html::script('/js/datatables.js') }}
+    {{ Html::script('/semantic/semantic.js') }}
+
+</head>
+<body>
 <div class="ui vertical inverted sticky menu bound bottom">
 
     <div class="item">
@@ -10,3 +32,5 @@
 </div>
 
 BONJOUR
+</body>
+</html>
