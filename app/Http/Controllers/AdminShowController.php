0b8d4205a49816824ef4f22817a8656e45d9b096
Youkoulayley <bmayelle@hotmail.fr>
2016-09-23 14:43:38 +0200

    Récupérer le token de l'API The TVDB

--- app/Http/Controllers/AdminShowController.php
+++ app/Http/Controllers/AdminShowController.php
@@ -81,7 +81,6 @@ class AdminShowController extends Controller
 
         $getShow = $client->request('GET', '/series/'. $theTVDBID, [
             'headers' => [
-                'Accept' => 'application/json',
                 'Accept' => 'application/vnd.thetvdb.v2.1.0',
                 'Authorization' => 'Bearer ' . $token,
             ]
