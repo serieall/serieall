36aef2b9bda03888eeac739871c009914b96690f
Youkoulayley <bmayelle@hotmail.fr>
2016-09-25 17:42:07 +0200

    Ajout de la fonction de remplacement des caractères spéciaux

--- app/Http/Controllers/Components/ReplaceSpecialsChars.php
+++ app/Http/Controllers/Components/ReplaceSpecialsChars.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Components;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Logging\Log;
 
 class ReplaceSpecialsChars extends Controller
 {
--- app/Jobs/AddShowFromTVDB.php
+++ app/Jobs/AddShowFromTVDB.php
@@ -121,9 +121,7 @@ class AddShowFromTVDB extends Job implements ShouldQueue
         $show_new->thetvdb_id = $theTVDBID; # L'ID de TheTVDB
         $show_new->name = $show->data->seriesName; # Le nom de la série
 
-        $nom_serie = $show_new->name;
-
-        $show_new->show_url = (new ReplaceSpecialsChars)->ReplaceSpecialsChars($nom_serie);
+        $show_new->show_url = (new ReplaceSpecialsChars($show_new->name))->ReplaceSpecialsChars($show_new->name);
 
         $show_new->save();
     }
