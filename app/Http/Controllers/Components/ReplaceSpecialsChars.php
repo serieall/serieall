45af416ad6e26adf219915633fe2694697d02aab
Youkoulayley <bmayelle@hotmail.fr>
2016-09-26 08:53:27 +0200

    Récupérer des informations sur les séries

--- app/Http/Controllers/Components/ReplaceSpecialsChars.php
+++ app/Http/Controllers/Components/ReplaceSpecialsChars.php
@@ -7,18 +7,5 @@ use Illuminate\Contracts\Logging\Log;
 
 class ReplaceSpecialsChars extends Controller
 {
-    public function ReplaceSpecialsChars($chaine){
-        //  On supprime les espaces
-        $chaine=trim($chaine);
-        // On remplace les chaines de caractères dont on ne veut pas
-        $chaine= strtr($chaine,"ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ","aaaaaaaaaaaaooooooooooooeeeeeeeecciiiiiiiiuuuuuuuuynn");
 
-        // On dégage les caractères spéciaux
-        $chaine = preg_replace('/([^.a-z0-9]+)/i', '-', $chaine);
-        // On met la chaine de caractères en minuscule
-        $chaine = strtolower($chaine);
-
-        // On renvoie la chaine modifiée
-        return $chaine;
-    }
 }
