a5c3ed001e9cbb6518e50fd1eecdf8a03e64e2fd
Youkoulayley <bmayelle@hotmail.fr>
2016-09-26 14:26:55 +0200

    Récupérer des informations sur les séries

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -45,7 +45,7 @@
             </div>
     </div>
 
-    <div class="ui left fixed vertical menu">
+    <div class="ui left sticky fixed vertical menu">
         <div class="item">
             <img class="ui centered image" src="/images/logo_v2.png">
         </div>
@@ -77,9 +77,7 @@
         </a>
     </div>
 
-    <div class="ui segment">
-        @yield('content')
-    </div>
+    @yield('content')
 
     <script>
         @yield('scripts')
