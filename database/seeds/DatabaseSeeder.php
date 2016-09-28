f606642b2466eb1b77a8bbd7eddee6401ba55e21
Youkoulayley <bmayelle@hotmail.fr>
2016-09-28 11:47:28 +0200

    Création des nouvelles tables : Artists, Genres et liaisons avec la table Shows

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ArtistsTableSeeder::class);
         $this->call(GenresTableSeeder::class);
         $this->call(GenreShowTableSeeder::class);
         $this->call(ArtistShowTableSeeder::class);
