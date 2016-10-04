f89f21c25ee455895f7e017bb5ca7c320a0d47b5
Youkoulayley <bmayelle@hotmail.fr>
2016-10-04 18:47:44 +0200

    Ajout des épisodes

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -8,6 +8,7 @@ class Episode extends Model {
 
 	protected $table = 'episodes';
 	public $timestamps = true;
+	protected $fillable = array('thetvdb_id', 'numero', 'name', 'name_fr', 'resume', 'particularite', 'diffusion_us', 'diffusion_fr', 'ba', 'moyenne', 'nbnotes');
 
 	public function seasons()
 	{
