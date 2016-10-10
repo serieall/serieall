4fc03d39209b3fc6b122ec63397a060a91e3e805
Youkoulayley <bmayelle@hotmail.fr>
2016-10-10 23:48:43 +0200

    Email de reset de mot de passe

--- app/Providers/PasswordResetServiceProvider.php
+++ app/Providers/PasswordResetServiceProvider.php
@@ -2,24 +2,23 @@
 
 namespace App\Providers;
 
-use App\Repositories\Auth\PasswordRepository;
+use App\Repositories\Auth\PasswordRepositoryManager;
+use Illuminate\Auth\Passwords\PasswordResetServiceProvider as IlluminatePasswordResetServiceProvider;
 
-class PasswordResetServiceProvider extends \Illuminate\Auth\Passwords\PasswordResetServiceProvider
+class PasswordResetServiceProvider extends IlluminatePasswordResetServiceProvider
 {
-
+    /**
+     * Register the password broker instance.
+     *
+     * @return void
+     */
     protected function registerPasswordBroker()
     {
         $this->app->singleton('auth.password', function ($app) {
-            $tokens = $app['auth.password.tokens'];
-
-            $users = $app['auth']->driver()->getProvider();
-
-            $view = $app['config']['auth.password.email'];
-
-            return new PasswordBroker(
-                $tokens, $users, $app['mailer'], $view
-            );
+            return new PasswordRepositoryManager($app);
+        });
+        $this->app->bind('auth.password.broker', function ($app) {
+            return $app->make('auth.password')->broker();
         });
     }
-
 }
--- app/Repositories/Auth/PasswordRepository.php
+++ app/Repositories/Auth/PasswordRepository.php
@@ -6,10 +6,13 @@ use Illuminate\Auth\Passwords\PasswordBroker as IlluminatePasswordBroker;
 
 class PasswordRepository extends IlluminatePasswordBroker
 {
-    public function emailResetLink()
-    {
-        $view = $this->emailView;
 
+    public function emailResetLink(\Illuminate\Contracts\Auth\CanResetPassword $user, $token, \Closure $callback = null)
+    {
+        // We will use the reminder view that was given to the broker to display the
+        // password reminder e-mail. We'll pass a "token" variable into the views
+        // so that it may be displayed for an user to click for password reset.
+        $view = $this->emailView;
         return $this->mailer->queue($view, compact('token', 'user'), function ($m) use ($user, $token, $callback) {
             $m->to($user->getEmailForPasswordReset());
             if (! is_null($callback)) {
