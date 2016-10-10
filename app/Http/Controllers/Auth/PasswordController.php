3f205a8fc8d3e020015fb787fe4bd82e5b4f98b1
Youkoulayley <bmayelle@hotmail.fr>
2016-10-10 23:06:25 +0200

    Email de reset de mot de passe

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Closure;
 
 class PasswordController extends Controller
 {
@@ -33,5 +35,30 @@ class PasswordController extends Controller
     protected function getEmailSubject()
     {
         return property_exists($this, 'subject') ? $this->subject : 'Votre lien de réinitialisation de mot de passe';
+    }
+
+
+    /**
+     * Send the password reset link via e-mail.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $token
+     * @param  \Closure|null  $callback
+     * @return int
+     */
+    public function emailResetLink(CanResetPasswordContract $user, $token, Closure $callback = null)
+    {
+        // We will use the reminder view that was given to the broker to display the
+        // password reminder e-mail. We'll pass a "token" variable into the views
+        // so that it may be displayed for an user to click for password reset.
+        $view = $this->emailView;
+
+        return $this->mailer->queue($view, compact('token', 'user'), function ($m) use ($user, $token, $callback) {
+            $m->to($user->getEmailForPasswordReset());
+
+            if (! is_null($callback)) {
+                call_user_func($callback, $m, $user, $token);
+            }
+        });
     }
 }
