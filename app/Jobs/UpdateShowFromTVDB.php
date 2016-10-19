8901ed5695eddcff6599574df315bd5135284dcf
Youkoulayley <bmayelle@hotmail.fr>
2016-10-19 13:37:10 +0200

    debug verif

--- app/Jobs/UpdateShowFromTVDB.php
+++ app/Jobs/UpdateShowFromTVDB.php
@@ -295,8 +295,9 @@ class UpdateShowFromTVDB extends Job implements ShouldQueue
                                     ->where('shows.thetvdb_id', $idSerie)
                                     ->where('artistables.profession', 'actor')
                                     ->where('artistables.role', 'TBA')
-                                    ->get()
+                                    ->pluck('artistable.id')
                                     ->toArray();
+
                                 Log::info($actor_role);
 
                                 if(!empty($actor_role)){
@@ -304,7 +305,7 @@ class UpdateShowFromTVDB extends Job implements ShouldQueue
                                     if($actorRole != 'TBA'){
                                         # On met à jour le rôle
                                         Log::info('L\'acteur ' . $actor . ' est déjà lié à la série mais son rôle n\'était pas rempli.');
-                                        $actor_ref->shows()->updateExistingPivot($actor_role['artistables.id'], ['role' => $actorRole]);
+                                        $serieInBDD->artists()->updateExistingPivot($actor_role['artistables.id'], ['role' => $actorRole]);
                                     }
                                 }
                             }
