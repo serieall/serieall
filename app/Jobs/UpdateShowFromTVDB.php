6d70b7ba729b5da2abc0e24ce90839fb975ec616
Youkoulayley <bmayelle@hotmail.fr>
2016-10-19 10:46:10 +0200

    Ajout de message Log

--- app/Jobs/UpdateShowFromTVDB.php
+++ app/Jobs/UpdateShowFromTVDB.php
@@ -277,7 +277,7 @@ class UpdateShowFromTVDB extends Job implements ShouldQueue
                         if(!is_null($actor_ref)) {
                             # On vérifie s'il est déjà lié à la série
                             $actor_liaison = Artist::wherehas('shows', function ($query) use($idSerie) {
-                                $query->where('thetvdb_id', '==', $idSerie);
+                                $query->where('shows.thetvdb_id', '==', $idSerie);
                             })->get();
 
                             Log::info($actor_liaison);
