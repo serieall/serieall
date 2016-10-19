bb81b764d03976a2d5633f7eef8bc35077046aaf
Youkoulayley <bmayelle@hotmail.fr>
2016-10-19 13:19:43 +0200

    debug verif

--- app/Jobs/UpdateShowFromTVDB.php
+++ app/Jobs/UpdateShowFromTVDB.php
@@ -303,7 +303,7 @@ class UpdateShowFromTVDB extends Job implements ShouldQueue
                                     if($actorRole != 'TBA'){
                                         # On met à jour le rôle
                                         Log::info('L\'acteur ' . $actor . ' est déjà lié à la série mais son rôle n\'était pas rempli.');
-                                        $serieInBDD->artists()->updateExistingPivot($actor_role['artistables.id'], ['role' => $actorRole]);
+                                        $serieInBDD->artists()->updateExistingPivot($actor_role['artistables.artist_id'], ['role' => $actorRole]);
                                     }
                                 }
                             }
