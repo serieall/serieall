2683414a353e6de73aabea20ca8980631c77fc8d
Youkoulayley <bmayelle@hotmail.fr>
2016-10-20 19:30:15 +0200

    Upgrade laravel 5.2 => 5.3²

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable {
-
+    use Notifiable;
 	protected $table = 'users';
 	public $timestamps = true;
 	protected $fillable = array('username', 'email', 'password');
