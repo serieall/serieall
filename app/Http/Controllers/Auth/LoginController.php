8223d72389f893607a3d3482e2e2b9c564010810
Youkoulayley <bmayelle@hotmail.fr>
2016-10-20 15:46:21 +0200

    Upgrade laravel 5.2 => 5.3

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,11 @@
 namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Dotenv\Validator;
+use App\Http\Requests\Request;
+use App\Services\ActivationService;
+use App\Packages\Hashing\YourHasher;
+
 class LoginController extends Controller
 {
     /*
@@ -20,14 +25,122 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
+    protected $activationService;
+    protected $hashingProvider;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(ActivationService $activationService, YourHasher $hashingProvider)
     {
         $this->middleware('guest', ['except' => 'logout']);
+        $this->activationService = $activationService;
+        $this->hashingProvider = $hashingProvider;
+    }
+
+    /**
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  bool  $throttles
+     * @return \Illuminate\Http\Response
+     */
+    protected function handleUserWasAuthenticated(Request $request, $throttles)
+    {
+        # Si le mot de passe n'st pas hashé avec Bcrypt
+        if($this->hashingProvider->needsRehash(Auth::user()->password)){
+            # On regénre un mot de passe avevc Bcrypt
+            Auth::user()->password = $this->hashingProvider->make($request->password);
+            # On sauvegarde l'utilisateur
+            Auth::user()->save();
+        }
+
+        if ($throttles) {
+            $this->clearLoginAttempts($request);
+        }
+
+        if (method_exists($this, 'authenticated')) {
+            return $this->authenticated($request, Auth::guard($this->getGuard())->user());
+        }
+
+        return redirect()->intended($this->redirectPath());
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     * Surcharge de la fonction de base de Laravel
+     *
+     * @return string
+     */
+    public function loginUsername()
+    {
+        return property_exists($this, 'username') ? $this->username : 'username';
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'username' => 'required|max:255|unique:users',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:8|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = $this->validator($request->all());
+
+        if ($validator->fails()) {
+            $this->throwValidationException(
+                $request, $validator
+            );
+        }
+
+        $user = $this->create($request->all());
+
+        $this->activationService->sendActivationMail($user);
+
+        return redirect('/login')->with('status', 'Nous vous avons envoyé un e-mail de confirmation.');
+    }
+
+    public function authenticated(Request $request, $user)
+    {
+        if (! $user->activated) {
+            $this->activationService->sendActivationMail($user);
+            auth()->logout();
+            return back()->with('warning', 'Vous devez valider votre adresse E-mail. Nous vous avons envoyé un code de validation.');
+        }
+        return redirect()->intended($this->redirectPath());
+    }
+
+    public function activateUser($token)
+    {
+        if ($user = $this->activationService->activateUser($token)) {
+            return redirect()->back();
+        }
+        abort(404);
     }
 }
