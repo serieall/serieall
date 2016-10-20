acd6348c7f325fad8a07693686d6d718c95ae3a3
Youkoulayley <bmayelle@hotmail.fr>
2016-10-20 16:20:52 +0200

    Upgrade laravel 5.2 => 5.3

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Http\Controllers\Auth;
-use App\User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
