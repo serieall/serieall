16fd26ae4b375f24f461c66e74ac991846cc906c
Youkoulayley <bmayelle@hotmail.fr>
2016-10-21 11:35:59 +0200

    Add Show Manually

--- resources/views/admin/shows/indexShows.blade.php
+++ resources/views/admin/shows/indexShows.blade.php
@@ -105,27 +105,28 @@
             </div>
         </div>
     </div>
+
+        <script>
+            $('#table-show-admin').DataTable( {
+                "order": [[ 0, "asc" ]],
+                "language": {
+                    "lengthMenu": "Afficher _MENU_ enregistrements par page",
+                    "zeroRecords": "Aucun enregistrement trouvé",
+                    "info": "Page _PAGE_ sur _PAGES_",
+                    "infoEmpty": "Aucun enregistrement trouvé",
+                    "infoFiltered": "(filtré sur _MAX_ enregistrements)",
+                    "sSearch" : "",
+                    "oPaginate": {
+                        "sFirst":    	"Début",
+                        "sPrevious": 	"Précédent",
+                        "sNext":     	"Suivant",
+                        "sLast":     	"Fin"
+                    }
+                }} );
+
+            $('#add-serie')
+                    .modal('show')
+            ;
+        </script>
 @endsection
 
-@section('scripts')
-        $('#table-show-admin').DataTable( {
-            "order": [[ 0, "asc" ]],
-            "language": {
-                "lengthMenu": "Afficher _MENU_ enregistrements par page",
-                "zeroRecords": "Aucun enregistrement trouvé",
-                "info": "Page _PAGE_ sur _PAGES_",
-                "infoEmpty": "Aucun enregistrement trouvé",
-                "infoFiltered": "(filtré sur _MAX_ enregistrements)",
-                "sSearch" : "",
-                "oPaginate": {
-                    "sFirst":    	"Début",
-                    "sPrevious": 	"Précédent",
-                    "sNext":     	"Suivant",
-                    "sLast":     	"Fin"
-            }
-        }} );
-
-        $('#add-serie')
-            .modal('show')
-        ;
-@endsection
