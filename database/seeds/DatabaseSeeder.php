4d2893d38a72bfb6682ead9a46c56dc55d91b075
Baptiste Mayelle <baptiste.mayelle@pingflow.fr>
2017-06-07 14:01:04 +0200

    add seeder for admin users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(TempsSeeder::class);
+        $this->call(UsersSeeder::class);
     }
 }
---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class UsersSeeder extends Seeder
+{
+
+    /**
+     * Génère une date aléatoire
+     *
+     * @return mixed
+     */
+    private function randDate()
+    {
+        return Carbon::createFromDate(null, rand(1, 12), rand(1, 28));
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $date = $this->randDate();
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'user_url' => 'admin',
+            'email' => 'bmayelle@hotmail.fr',
+            'password' => '$2y$10$aO/pp1im2mg9sO9TonUFbu85ng4LZMZRCn8eft9wRq/QDYBQRPnYe',
+            'role' => 1,
+            'activated' => 1,
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+    }
+}
