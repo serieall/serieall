453811a9ddd8fc7a9219ea3635391521aa608188
Baptiste Mayelle <baptiste.mayelle@pingflow.fr>
2017-08-30 13:59:10 +0200

    episode suivant & precedent done

--- resources/views/shows/episodes.blade.php
+++ resources/views/shows/episodes.blade.php
@@ -10,7 +10,7 @@
                 </a>
                 <a class="active item">
                     <i class="big browser icon"></i>
-                    Saisons
+                    Saison {{ $seasonInfo->name }}
                 </a>
                 <a class="item" href="{{ route('show.details', $showInfo['show']->show_url) }}">
                     <i class="big list icon"></i>
@@ -43,25 +43,69 @@
 
                         @foreach($seasonInfo->episodes as $index => $episode)
                             @if($episode->id == $episodeInfo->id)
-                                {{ $index }}
                                 @if($index == 0)
-                                    <a class="item" href="">
+                                    <?php
+                                        $numeroEpisodeSuivant = $seasonInfo->episodes[$index + 1]->numero;
+                                         $IDEpisodeSuivant = $seasonInfo->episodes[$index + 1]->id;
+                                    ?>
+                                    <a class="item" href="
+                                        @if($numeroEpisodeSuivant == 0)
+                                            {{ route('show.episodes', [$showInfo['show']->show_url, $seasonInfo->name, $numeroEpisodeSuivant, $IDEpisodeSuivant]) }}
+                                        @else
+                                            {{ route('show.episodes', [$showInfo['show']->show_url, $seasonInfo->name, $numeroEpisodeSuivant]) }}
+                                        @endif
+                                    ">
+                                        Episode suivant
+                                        <i class="right chevron icon"></i>
+                                    </a>
+                                @elseif($index == $totalEpisodes )
+                                    <?php
+                                        $numeroEpisodePrecedent = $seasonInfo->episodes[$index - 1]->numero;
+                                         $IDEpisodePrecedent = $seasonInfo->episodes[$index - 1]->id;
+                                    ?>
+                                    <a class="item" href="
+                                        @if($numeroEpisodePrecedent == 0)
+                                            {{ route('show.episodes', [$showInfo['show']->show_url, $seasonInfo->name, $numeroEpisodePrecedent, $IDEpisodePrecedent]) }}
+                                        @else
+                                            {{ route('show.episodes', [$showInfo['show']->show_url, $seasonInfo->name, $numeroEpisodePrecedent]) }}
+                                        @endif
+                                    ">
                                         <i class="left chevron icon"></i>
                                         Episode précédent
                                     </a>
-                                @elseif($index == $totalEpisodes )
-                                    Dernier
                                 @else
-                                    Entre deux
+                                    <?php
+                                        $numeroEpisodePrecedent = $seasonInfo->episodes[$index - 1]->numero;
+                                        $IDEpisodePrecedent = $seasonInfo->episodes[$index - 1]->id;
+                                    ?>
+                                    <a class="item" href="
+                                        @if($numeroEpisodePrecedent == 0)
+                                            {{ route('show.episodes', [$showInfo['show']->show_url, $seasonInfo->name, $numeroEpisodePrecedent, $IDEpisodePrecedent]) }}
+                                        @else
+                                            {{ route('show.episodes', [$showInfo['show']->show_url, $seasonInfo->name, $numeroEpisodePrecedent]) }}
+                                        @endif
+                                    ">
+                                        <i class="left chevron icon"></i>
+                                        Episode précédent
+                                    </a>
+
+                                    <?php
+                                        $numeroEpisodeSuivant = $seasonInfo->episodes[$index + 1]->numero;
+                                        $IDEpisodeSuivant = $seasonInfo->episodes[$index + 1]->id;
+                                    ?>
+                                    <a class="item" href="
+                                        @if($numeroEpisodeSuivant == 0)
+                                            {{ route('show.episodes', [$showInfo['show']->show_url, $seasonInfo->name, $numeroEpisodeSuivant, $IDEpisodeSuivant]) }}
+                                        @else
+                                            {{ route('show.episodes', [$showInfo['show']->show_url, $seasonInfo->name, $numeroEpisodeSuivant]) }}
+                                        @endif
+                                    ">
+                                        Episode suivant
+                                        <i class="right chevron icon"></i>
+                                    </a>
                                 @endif
                             @endif
                         @endforeach
-
-
-                        <a class="item" href="{{ route('show.seasons', [$showInfo['show']->show_url, $seasonInfo->name]) }}">
-                            Episode suivant
-                            <i class="right chevron icon"></i>
-                        </a>
                     </div>
                 </div>
             </div>
@@ -70,109 +114,195 @@
 @endsection
 
 @section('content_fiche_left')
-    <div id="episodeDetails" class="ui segment">
-        <h1>
-            @if(!empty($episodeInfo->name_fr))
-                {{ $episodeInfo->name_fr }}
-            @else
-                {{ $episodeInfo->name }}
-            @endif
-        </h1>
-        <h2 class="ui episode titreen">
-            @if(!empty($episodeInfo->name_fr))
-                {{ $episodeInfo->name }}
-            @endif
-        </h2>
-        <p>
-            @if(empty($episodeInfo->resume_fr))
-                @if(empty($episodeInfo->resume))
-                    Pas de résumé pour l'instant ...
-                @else
-                    {{ $episodeInfo->resume }}
-                @endif
-            @else
-                {{ $episodeInfo->resume_fr }}
-            @endif
-        </p>
-        <div class="ui divider"></div>
+    <div class="ui stackable grid">
+        <div class="row">
+            <div id="episodeDetails" class="ui segment">
+                <h1>
+                    @if(!empty($episodeInfo->name_fr))
+                        {{ $episodeInfo->name_fr }}
+                    @else
+                        {{ $episodeInfo->name }}
+                    @endif
+                </h1>
+                <h2 class="ui episode titreen">
+                    @if(!empty($episodeInfo->name_fr))
+                        {{ $episodeInfo->name }}
+                    @endif
+                </h2>
+                <p>
+                    @if(empty($episodeInfo->resume_fr))
+                        @if(empty($episodeInfo->resume))
+                            Pas de résumé pour l'instant ...
+                        @else
+                            {{ $episodeInfo->resume }}
+                        @endif
+                    @else
+                        {{ $episodeInfo->resume_fr }}
+                    @endif
+                </p>
+                <div class="ui divider"></div>
 
-        <div class="ui top attached tabular menu">
-            <a class="active item" data-tab="first" >Informations sur l'épisode</a>
-            <a class="item" data-tab="second">Fiche technique</a>
-        </div>
-        <div class="ui bottom attached active tab segment" data-tab="first">
-            @if(!empty($episodeInfo->picture))
-                <div class="ui center aligned">
-                    <img src="{{ $episodeInfo->picture }}">
+                <div class="ui top attached tabular menu">
+                    <a class="active item" data-tab="first" >Informations sur l'épisode</a>
+                    <a class="item" data-tab="second">Fiche technique</a>
                 </div>
-            @endif
-            <table class="ui center aligned table">
-                <thead>
-                <tr>
-                    @if($episodeInfo->diffusion_us != '0000-00-00')
-                        <th>
-                            <i class="calendar icon"></i>
-                            Diffusion originale
-                        </th>
+                <div class="ui bottom attached active tab segment" data-tab="first">
+                    @if(!empty($episodeInfo->picture))
+                        <div class="ui center aligned">
+                            <img src="{{ $episodeInfo->picture }}">
+                        </div>
                     @endif
-                    @if($episodeInfo->diffusion_fr != '0000-00-00')
-                        <th>
-                            <i class="calendar icon"></i>
-                            Diffusion française
-                        </th>
-                    @endif
-                </tr>
-                </thead>
-                <tr>
-                    @if($episodeInfo->diffusion_us != '0000-00-00')
-                        <td>
-                            {{ $episodeInfo->diffusion_us }}
-                        </td>
-                    @endif
-                    @if($episodeInfo->diffusion_fr != '0000-00-00')
-                        <td>
-                            {{ $episodeInfo->diffusion_fr }}
-                        </td>
-                    @endif
-                </tr>
-            </table>
+                    <table class="ui center aligned table">
+                        <thead>
+                        <tr>
+                            @if($episodeInfo->diffusion_us != '0000-00-00')
+                                <th>
+                                    <i class="calendar icon"></i>
+                                    Diffusion originale
+                                </th>
+                            @endif
+                            @if($episodeInfo->diffusion_fr != '0000-00-00')
+                                <th>
+                                    <i class="calendar icon"></i>
+                                    Diffusion française
+                                </th>
+                            @endif
+                        </tr>
+                        </thead>
+                        <tr>
+                            @if($episodeInfo->diffusion_us != '0000-00-00')
+                                <td>
+                                    {{ $episodeInfo->diffusion_us }}
+                                </td>
+                            @endif
+                            @if($episodeInfo->diffusion_fr != '0000-00-00')
+                                <td>
+                                    {{ $episodeInfo->diffusion_fr }}
+                                </td>
+                            @endif
+                        </tr>
+                    </table>
+                </div>
+                <div class="ui bottom attached tab segment" data-tab="second">
+                    <table class="ui table">
+                        <thead>
+                        <tr>
+                            <th>
+                                Réalisateurs
+                            </th>
+                            <th>
+                                Scénaristes
+                            </th>
+                            <th>
+                                Guests
+                            </th>
+                        </tr>
+                        </thead>
+                        <tr>
+                            <td>
+                                @foreach($episodeInfo->directors as $director)
+                                    {{ $director->name }}
+                                    <br />
+                                @endforeach
+                            </td>
+                            <td>
+                                @foreach($episodeInfo->writers as $writer)
+                                    {{ $writer->name }}
+                                    <br />
+                                @endforeach
+                            </td>
+                            <td>
+                                @foreach($episodeInfo->guests as $guest)
+                                    {{ $guest->name }}
+                                    <br />
+                                @endforeach
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </div>
-        <div class="ui bottom attached tab segment" data-tab="second">
-            <table class="ui table">
-                <thead>
-                <tr>
-                    <th>
-                        Réalisateurs
-                    </th>
-                    <th>
-                        Scénaristes
-                    </th>
-                    <th>
-                        Guests
-                    </th>
-                </tr>
-                </thead>
-                <tr>
-                    <td>
-                        @foreach($episodeInfo->directors as $director)
-                            {{ $director->name }}
-                            <br />
-                        @endforeach
-                    </td>
-                    <td>
-                        @foreach($episodeInfo->writers as $writer)
-                            {{ $writer->name }}
-                            <br />
-                        @endforeach
-                    </td>
-                    <td>
-                        @foreach($episodeInfo->guests as $guest)
-                            {{ $guest->name }}
-                            <br />
-                        @endforeach
-                    </td>
-                </tr>
-            </table>
+        <div class="row">
+            <div id="ListAvis" class="ui segment">
+                <h1>Derniers avis sur l'épisode</h1>
+                <div class="ui stackable grid">
+                    <div class="row">
+                        <div class="center aligned three wide column">
+                            <img class="ui tiny image" src="{{ Gravatar::src('bmayelle@hotmail.fr') }}">
+                            <span>Youkoulayley</span><br />
+                            <span class="ui red text">Administrateur</span>
+                        </div>
+                        <div class="AvisBox center aligned twelve wide column">
+                            <table class="ui grey left border table">
+                                <tr>
+                                    <td class="ui grey text AvisStatus">Avis neutre</td>
+                                    <td class="right aligned">Déposé le 26/05/2017 à 9h53</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" class="AvisResume">
+                                        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+                                            dicta dolorem excepturi non nulla, obcaecati quo rerum sequi sint? Ab
+                                            cumque deserunt doloribus iste, molestias provident quia repellat
+                                            repellendus soluta?
+                                        </div>
+                                        <div>A culpa eius esse laboriosam neque nobis odio, sapiente. Aliquam
+                                            animi, at consectetur earum eos itaque iure modi nisi nulla odio
+                                            perspiciatis ...
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="ui grey text">10 réponses</td>
+                                    <td class="LireAvis"><a>Lire l'avis complet ></a></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="center aligned three wide column">
+                            <img class="ui tiny image" src="{{ Gravatar::src('bmayelle@hotmail.fr') }}">
+                            <span>Youkoulayley</span><br />
+                            <span class="ui red text">Administrateur</span>
+                        </div>
+                        <div class="AvisBox center aligned twelve wide column">
+                            <table class="ui green left border table">
+                                <tr>
+                                    <td class="ui green text AvisStatus">Avis favorable</td>
+                                    <td class="right aligned">Déposé le 26/05/2017 à 9h53</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" class="AvisResume">
+                                        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+                                            dicta dolorem excepturi non nulla, obcaecati quo rerum sequi sint? Ab
+                                            cumque deserunt doloribus iste, molestias provident quia repellat
+                                            repellendus soluta?
+                                        </div>
+                                        <div>A culpa eius esse laboriosam neque nobis odio, sapiente. Aliquam
+                                            animi, at consectetur earum eos itaque iure modi nisi nulla odio
+                                            perspiciatis ...
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="ui grey text">10 réponses</td>
+                                    <td class="LireAvis"><a>Lire l'avis complet ></a></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="three wide column">
+
+                        </div>
+                        <div class="twelve wide column">
+                            <div class="ui DarkBlueSerieAll button">
+                                <i class="write icon"></i> Ecrire un avis
+                            </div>
+                            <a class="AllAvis" href="#"><p>Toutes les avis ></p></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
