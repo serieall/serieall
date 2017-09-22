cd9868ada6e3ea68934f7f41b3edb67703267e39
Baptiste Mayelle <baptiste.mayelle@pingflow.fr>
2017-09-22 13:35:38 +0200

    change escape method blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
         View::share('noteGood', $noteGood);
         View::share('noteNeutral', $noteNeutral);
         View::share('noteBad', $noteBad);
+
+        Blade::setEchoFormat('e(utf8_encode(%s))');
     }
 
     /**
