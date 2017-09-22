14eb9f010f41330c526077f1dbd0a97005aea967
Baptiste Mayelle <baptiste.mayelle@pingflow.fr>
2017-09-22 16:53:07 +0200

    fix spoiler

--- public/spoiler/spoiler.js
+++ public/spoiler/spoiler.js
@@ -1,8 +1,11 @@
 $(function() {
+    // Hide Spoiler by default
+    $('div.spoiler-title').children().first().attr('class', 'spoiler-toggle show-icon');
+
 	$('div.spoiler-title').click(function() {
 		$(this)
 			.children()
-			.first()
+            .first()
 			.toggleClass('show-icon')
 			.toggleClass('hide-icon');
 		$(this)
