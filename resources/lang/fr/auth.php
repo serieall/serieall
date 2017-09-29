11c16ec0308e5a5213e0e69ff0fd094287c20278
Baptiste Mayelle <baptiste.mayelle@pingflow.fr>
2017-09-29 12:02:03 +0200

    add lang fr

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements',
+    'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements.',
     'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
 
 ];
