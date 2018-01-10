891c14e628a4846919800d2209797e3551082951
Baptiste Mayelle <baptiste.mayelle@pingflow.fr>
2018-01-10 17:41:08 +0100

    update form create article

--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -25,7 +25,7 @@
         <div class="fifteen wide column segment">
             <div class="ui segment">
                 <form class="ui form" method="POST" action="">
-                    <div class="ui field">
+                    <div class="ui required field">
                         <label for="categories">Choisir la catégorie de l'article</label>
                         <div id="dropdownCategories" class="ui fluid search selection dropdown">
                             <input name="categories" type="hidden">
@@ -35,35 +35,89 @@
                             </div>
                         </div>
                     </div>
-                    <div class="ui field">
-                        <label for="show">Choisir la série liée</label>
-                        <div id="dropdownShows" class="ui search selection dropdown">
-                            <input id="inputShow" name="show" type="hidden">
-                            <i class="dropdown icon"></i>
-                            <div class="default text">Série</div>
-                            <div class="menu">
-                            </div>
-                        </div>
-                        <div class="ui inline button clearShow">Effacer</div>
+
+                    <div class="div-center margin1">
+                        <div id="oneShow" class="ui left attached BlueSerieAll button">Une série</div>
+                        <div id="multipleShows" class="ui right attached ui button">Plusieurs séries</div>
+                        <input type="hidden" name="one" value="1">
                     </div>
 
-                    <div class="ui field">
-                        <label for="show">Choisir la saison liée</label>
-                        <div id="dropdownSeasons" class="ui disabled fluid search selection dropdown">
-                            <input id="inputSeason" name="season" type="hidden">
-                            <i class="dropdown icon"></i>
-                            <div class="default text">Saison</div>
-                            <div class="menu">
+                    <div class="verticalDivider ui two column very relaxed grid">
+                        <div class="column">
+                            <div class="ui required field">
+                                <label for="show">Choisir la série liée</label>
+                                <div class="ui grid">
+                                    <div class="thirteen wide column">
+                                        <div class="ui search fluid selection dropdown oneShowField dropdownShows">
+                                            <input id="inputShow" name="show" type="hidden">
+                                            <i class="dropdown icon"></i>
+                                            <div class="default text">Série</div>
+                                            <div class="menu">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="three wide column">
+                                        <div class="ui inline button clearShow oneShowField">Effacer</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="ui field">
+                                <label for="show">Choisir la saison liée</label>
+                                <div class="ui grid">
+                                    <div class="thirteen wide column">
+                                        <div id="dropdownSeasons" class="ui disabled fluid search selection dropdown">
+                                            <input id="inputSeason" name="season" type="hidden">
+                                            <i class="dropdown icon"></i>
+                                            <div class="default text">Saison</div>
+                                            <div class="menu">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="three wide column">
+                                        <div class="ui inline disabled button clearSeason">Effacer</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="ui field">
+                                <label for="show">Choisir l'épisodes lié</label>
+                                <div class="ui grid">
+                                    <div class="thirteen wide column">
+                                        <div id="dropdownEpisodes" class="ui disabled fluid search selection dropdown">
+                                            <input id="inputEpisode" name="episode" type="hidden">
+                                            <i class="dropdown icon"></i>
+                                            <div class="default text">Episode</div>
+                                            <div class="menu">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="three wide column">
+                                        <div class="ui inline disabled button clearEpisode">Effacer</div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="ui field">
-                        <label for="show">Choisir l'épisodes lié</label>
-                        <div id="dropdownEpisodes" class="ui disabled fluid search selection dropdown">
-                            <input id="inputEpisode" name="episode" type="hidden">
-                            <i class="dropdown icon"></i>
-                            <div class="default text">Episode</div>
-                            <div class="menu">
+                        <div class="ui vertical divider">
+                            OU
+                        </div>
+                        <div class="column">
+                            <div class="ui required field">
+                                <label for="shows">Choisir la ou les série(s) liée(s)</label>
+                                <div class="ui grid">
+                                    <div class="thirteen wide column">
+                                        <div class="ui disabled fluid search multiple selection dropdown multipleShowsField dropdownShows">
+                                            <input id="inputShows" name="shows" type="hidden">
+                                            <i class="dropdown icon"></i>
+                                            <div class="default text">Série(s)</div>
+                                            <div class="menu">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="three wide column">
+                                        <div class="ui disabled inline button clearShows multipleShowsField">Effacer</div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -75,7 +129,7 @@
 
 @section('scripts')
     <script>
-        var dropdownShows = '#dropdownShows';
+        var dropdownShows = '.dropdownShows';
         var inputShow = '#inputShow';
         var inputSeason = '#inputSeason';
         var dropdownCategories = '#dropdownCategories';
@@ -97,9 +151,41 @@
                 })
             ;
 
-            $('.clearShow').click(function (e){
-                e.preventDefault();
-               $(dropdownShows).dropdown('restore defaults');
+            $('.clearShow').click(function (){
+               $(dropdownShows).dropdown('clear');
+            });
+
+            $('.clearSeason').click(function (){
+                $(dropdownSeasons).dropdown('clear');
+            });
+            $('.clearEpisode').click(function (){
+                $(dropdownEpisodes).dropdown('clear');
+            });
+
+            $('#oneShow').click(function () {
+                // We change the colors of buttons
+                $(this).addClass('BlueSerieAll');
+                $(this).next().removeClass('BlueSerieAll');
+
+                // We disabled the field linked to the not selected
+                $('.oneShowField').removeClass('disabled');
+                $('.multipleShowsField').addClass('disabled');
+
+                // We change the value of the hidden input
+                $('input[name=one]').val(1);
+            });
+
+            $('#multipleShows').click(function () {
+                // We change the colors of buttons
+                $(this).addClass('BlueSerieAll');
+                $(this).prev().removeClass('BlueSerieAll');
+
+                // We disabled the field linked to the not selected
+                $('.oneShowField').addClass('disabled');
+                $('.multipleShowsField').removeClass('disabled');
+
+                // We change the value of the hidden input
+                $('input[name=one]').val(0);
             });
 
             // Init the dropdown Categories
@@ -135,9 +221,11 @@
                 // If Show is not empty, we enabled the dropdown
                 if(!$(inputShow).val()) {
                     $(dropdownSeasons).addClass('disabled');
+                    $(dropdownSeasons + '+div').addClass('disabled');
                 }
                 else {
                     $(dropdownSeasons).removeClass('disabled');
+                    $(dropdownSeasons + '+div').removeClass('disabled');
                 }
             });
 
@@ -162,9 +250,11 @@
                 // If Season is not empty, we enabled the dropdown
                 if(!$(inputSeason).val()) {
                     $(dropdownEpisodes).addClass('disabled');
+                    $(dropdownEpisodes + '+div').addClass('disabled');
                 }
                 else {
                     $(dropdownEpisodes).removeClass('disabled');
+                    $(dropdownEpisodes + '+div').removeClass('disabled');
                 }
             });
         });
