d3216e9ca8d1c6e75ef8dca4fb78e72afbf1fb9d
Youkoulayley <bmayelle@hotmail.fr>
2018-01-11 00:30:42 +0100

    continue form article create

--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -27,7 +27,7 @@
                 <form class="ui form" method="POST" action="">
                     <div class="ui required field">
                         <label for="categories">Choisir la catégorie de l'article</label>
-                        <div id="dropdownCategories" class="ui fluid search selection dropdown">
+                        <div class="ui fluid search selection dropdown dropdownCategory">
                             <input name="categories" type="hidden">
                             <i class="dropdown icon"></i>
                             <div class="default text">Catégorie</div>
@@ -42,13 +42,13 @@
                         <input type="hidden" name="one" value="1">
                     </div>
 
-                    <div class="verticalDivider ui two column very relaxed grid">
+                    <div class="verticalDivider ui two column very relaxed grid stackable">
                         <div class="column">
                             <div class="ui required field">
                                 <label for="show">Choisir la série liée</label>
                                 <div class="ui grid">
                                     <div class="thirteen wide column">
-                                        <div class="ui search fluid selection dropdown oneShowField dropdownShows">
+                                        <div class="ui search fluid selection dropdown oneShowField dropdownShow">
                                             <input id="inputShow" name="show" type="hidden">
                                             <i class="dropdown icon"></i>
                                             <div class="default text">Série</div>
@@ -66,7 +66,7 @@
                                 <label for="show">Choisir la saison liée</label>
                                 <div class="ui grid">
                                     <div class="thirteen wide column">
-                                        <div id="dropdownSeasons" class="ui disabled fluid search selection dropdown">
+                                        <div class="ui disabled fluid search selection dropdown oneShowField dropdownSeason">
                                             <input id="inputSeason" name="season" type="hidden">
                                             <i class="dropdown icon"></i>
                                             <div class="default text">Saison</div>
@@ -75,7 +75,7 @@
                                         </div>
                                     </div>
                                     <div class="three wide column">
-                                        <div class="ui inline disabled button clearSeason">Effacer</div>
+                                        <div class="ui inline disabled button oneShowField clearSeason">Effacer</div>
                                     </div>
                                 </div>
                             </div>
@@ -84,7 +84,7 @@
                                 <label for="show">Choisir l'épisodes lié</label>
                                 <div class="ui grid">
                                     <div class="thirteen wide column">
-                                        <div id="dropdownEpisodes" class="ui disabled fluid search selection dropdown">
+                                        <div class="ui disabled fluid search selection dropdown oneShowField dropdownEpisode">
                                             <input id="inputEpisode" name="episode" type="hidden">
                                             <i class="dropdown icon"></i>
                                             <div class="default text">Episode</div>
@@ -93,7 +93,7 @@
                                         </div>
                                     </div>
                                     <div class="three wide column">
-                                        <div class="ui inline disabled button clearEpisode">Effacer</div>
+                                        <div class="ui inline disabled button oneShowField clearEpisode">Effacer</div>
                                     </div>
                                 </div>
                             </div>
@@ -106,7 +106,7 @@
                                 <label for="shows">Choisir la ou les série(s) liée(s)</label>
                                 <div class="ui grid">
                                     <div class="thirteen wide column">
-                                        <div class="ui disabled fluid search multiple selection dropdown multipleShowsField dropdownShows">
+                                        <div class="ui disabled fluid search multiple selection dropdown multipleShowsField dropdownShow">
                                             <input id="inputShows" name="shows" type="hidden">
                                             <i class="dropdown icon"></i>
                                             <div class="default text">Série(s)</div>
@@ -121,6 +121,42 @@
                             </div>
                         </div>
                     </div>
+
+                    <div class="ui required field {{ $errors->has('name') ? ' error' : '' }}">
+                        <label for="nameInput">Titre de l'article</label>
+                        <input id="nameInput" name="name" value="{{ old('name') }}">
+
+                        @if ($errors->has('name'))
+                            <div class="ui red message">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </div>
+                        @endif
+                    </div>
+
+                    <div class="ui required field {{ $errors->has('intro') ? ' error' : '' }}">
+                        <label for="introInput">Chapô</label>
+                        <textarea id="introInput" rows="2"></textarea>
+
+                        @if ($errors->has('intro'))
+                            <div class="ui red message">
+                                <strong>{{ $errors->first('intro') }}</strong>
+                            </div>
+                        @endif
+                    </div>
+
+                    <div class="field">
+                        <div class="ui toggle checkbox">
+                            <input id="publishedInput" name="published" type="checkbox">
+                            <label for="publishedInput">Publier l'article</label>
+                        </div>
+                    </div>
+
+                    <div class="ui toggle checkbox">
+                        <input id="uneInput" name="une" type="checkbox">
+                        <label for="uneInput">Mettre l'article en une</label>
+                    </div>
+
+
                 </form>
             </div>
         </div>
@@ -129,39 +165,48 @@
 
 @section('scripts')
     <script>
-        var dropdownShows = '.dropdownShows';
+
+        $('.ui.toggle.checkbox').checkbox();
+
+        var dropdownCategory = '.dropdownCategory';
+        var dropdownShow = '.dropdownShow';
+        var dropdownSeason = '.dropdownSeason';
+        var dropdownEpisode = '.dropdownEpisode';
+
         var inputShow = '#inputShow';
         var inputSeason = '#inputSeason';
-        var dropdownCategories = '#dropdownCategories';
-        var dropdownSeasons = '#dropdownSeasons';
-        var dropdownEpisodes = '#dropdownEpisodes';
+
+        var clearSeasonButton = '.clearSeason';
+        var clearEpisodeButton = '.clearEpisode';
 
         $(document).ready(function() {
-            // Init the dropdown Show
-            $(dropdownShows)
-                .dropdown({
-                    apiSettings: {
-                        url: '/api/shows/list?name-lk=*{query}*'
-                    },
-                    fields: {
-                        remoteValues: "data",
-                        value: "id"
-                    },
-                    saveRemoteData: false
-                })
-            ;
 
-            $('.clearShow').click(function (){
-               $(dropdownShows).dropdown('clear');
-            });
+            function checkShowState() {
+                // If Show is not empty, we enabled the dropdown
+                if(!$(inputShow).val()) {
+                    $(dropdownSeason).addClass('disabled');
+                    $(clearSeasonButton).addClass('disabled');
+                }
+                else {
+                    $(dropdownSeason).removeClass('disabled');
+                    $(clearSeasonButton).removeClass('disabled');
+                }
+            }
 
-            $('.clearSeason').click(function (){
-                $(dropdownSeasons).dropdown('clear');
-            });
-            $('.clearEpisode').click(function (){
-                $(dropdownEpisodes).dropdown('clear');
-            });
+            function checkSeasonState() {
+                // If Season is not empty, we enabled the dropdown
+                if(!$(inputSeason).val()) {
+                    $(dropdownEpisode).addClass('disabled');
+                    $(clearEpisodeButton).addClass('disabled');
+                }
+                else {
+                    $(dropdownEpisode).removeClass('disabled');
+                    $(clearEpisodeButton).removeClass('disabled');
+                }
+            }
 
+
+            // On click on "One Show"
             $('#oneShow').click(function () {
                 // We change the colors of buttons
                 $(this).addClass('BlueSerieAll');
@@ -173,8 +218,12 @@
 
                 // We change the value of the hidden input
                 $('input[name=one]').val(1);
+
+                checkShowState();
+                checkSeasonState();
             });
 
+            // On click on "Multiple Show"
             $('#multipleShows').click(function () {
                 // We change the colors of buttons
                 $(this).addClass('BlueSerieAll');
@@ -189,7 +238,7 @@
             });
 
             // Init the dropdown Categories
-            $(dropdownCategories)
+            $(dropdownCategory)
                 .dropdown({
                     apiSettings: {
                         url: '/api/categories/list?name-lk=*{query}*'
@@ -201,11 +250,39 @@
                 })
             ;
 
+            // Clear buttons
+            $('.clearShow').click(function () {
+                $(dropdownShow).dropdown('clear');
+            });
+            $('.clearSeason').click(function (){
+                $(dropdownSeason).dropdown('clear');
+            });
+            $('.clearEpisode').click(function (){
+                $(dropdownEpisode).dropdown('clear');
+            });
+            $('.clearShows').click(function (){
+               $(dropdownShow + '.multipleShowsField').dropdown('clear');
+            });
+
+            // Init the dropdown Show
+            $(dropdownShow)
+                .dropdown({
+                    apiSettings: {
+                        url: '/api/shows/list?name-lk=*{query}*'
+                    },
+                    fields: {
+                        remoteValues: "data",
+                        value: "id"
+                    },
+                    saveRemoteData: false
+                })
+            ;
+
             // On change on Show, we init the dropdown Seasons with the new value of show
             $(inputShow).change( function() {
                 var show =  $(inputShow).val();
 
-                $(dropdownSeasons)
+                $(dropdownSeason)
                     .dropdown({
                         apiSettings: {
                             url: '/api/seasons/show/'+ show
@@ -218,22 +295,14 @@
                     .dropdown('clear')
                 ;
 
-                // If Show is not empty, we enabled the dropdown
-                if(!$(inputShow).val()) {
-                    $(dropdownSeasons).addClass('disabled');
-                    $(dropdownSeasons + '+div').addClass('disabled');
-                }
-                else {
-                    $(dropdownSeasons).removeClass('disabled');
-                    $(dropdownSeasons + '+div').removeClass('disabled');
-                }
+                checkShowState();
             });
 
             // On change on Season, we init the dropdown Episodes with the new value of season
             $(inputSeason).change( function() {
                 var season =  $(inputSeason).val();
 
-                $(dropdownEpisodes)
+                $(dropdownEpisode)
                     .dropdown({
                         apiSettings: {
                             url: '/api/episodes/seasons/' + season
@@ -247,15 +316,7 @@
                     .dropdown('clear')
                 ;
 
-                // If Season is not empty, we enabled the dropdown
-                if(!$(inputSeason).val()) {
-                    $(dropdownEpisodes).addClass('disabled');
-                    $(dropdownEpisodes + '+div').addClass('disabled');
-                }
-                else {
-                    $(dropdownEpisodes).removeClass('disabled');
-                    $(dropdownEpisodes + '+div').removeClass('disabled');
-                }
+                checkSeasonState();
             });
         });
     </script>
