d5c513fda3805d8890a53ddebfdb71e0e5671d18
Youkoulayley <bmayelle@hotmail.fr>
2018-11-11 11:34:53 +0100

    upd: migrations for show_user

---
+++ database/migrations/2018_11_11_103018_update_foreign_keys_show_user.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateForeignKeysShowUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('show_user', function ($table) {
+            $table->dropForeign('show_user_shows_id_fk');
+        });
+
+        Schema::table('show_user', function ($table) {
+            $table->dropForeign('show_user_users_id_fk');
+        });
+
+        Schema::table('show_user', function(Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+        });
+
+        Schema::table('show_user', function(Blueprint $table) {
+            $table->foreign('show_id')->references('id')->on('shows')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
