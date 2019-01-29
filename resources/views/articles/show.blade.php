d1ca93fdb3084a383b1ad54ad594d6f34be36ebf
Youkoulayley <bmayelle@hotmail.fr>
2019-01-29 21:51:31 +0100

    upd: split js articles

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -214,4 +214,5 @@
 @push('scripts')
     <script src="/js/article.show.js"></script>
     <script src="/js/views/comments/store_reaction.js"></script>
+    <script src="/js/views/comments/store_comment.js"></script>
 @endpush
