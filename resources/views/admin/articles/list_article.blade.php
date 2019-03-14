01058449aefd3a0089b66d176da2d937a577f73a
elpiolito <jojo.jumper@hotmail.fr>
2019-03-14 10:36:29 +0100

    Issue #144 - Refactoring admin view to display last 10 elements

--- resources/views/admin/articles/list_article.blade.php
+++ resources/views/admin/articles/list_article.blade.php
@@ -1,43 +1,45 @@
 @component('components.tables.table_admin', ['headers' => ["Titre de l'article", "Chapô", "Catégorie", "Auteur(s)", "Date d'écriture", "Date de publication", "Actions"]])
-    <tr class="line">
-        <td>
-            <a href="{{ route('admin.articles.edit', $article->id) }}" title="Modifier l'article">{{ $article->name }}</a>
-        </td>
-        <td>
-            {{ $article->intro }}
-        </td>
-        <td>
-            {{ $article->category->name }}
-        </td>
-        <td>
-            @foreach($article->users as $autor)
-                {{ $autor->username }}
-            @endforeach
-        </td>
-        <td>
-            {{ $article->created_at }}
-        </td>
-        <td>
-            {{ $article->published_at }}
-        </td>
-        <td class="center aligned">
-            <div class="four wide column">
-                <div class="flex-center">
-                    <button class="circular ui blue icon button" title="Voir l'aperçu de l'article" onclick="window.open('{{ route('article.show', $article->article_url) }}')">
-                        <i class="icon eye"></i>
-                    </button>
-                    <!-- Formulaire de suppression -->
-                    <form action="{{ route('admin.articles.destroy', $article->id) }}" method="post" >
-                        {{ csrf_field() }}
-
-                        <input type="hidden" name="_method" value="DELETE">
-
-                        <button class="circular ui red icon button" title="Supprimer l'article" value="Supprimer cet article ?" onclick="return confirm('Voulez-vous vraiment supprimer cet article ?')">
-                            <i class="icon remove"></i>
+    @for ($i = (count($articles)-1) ; $i > count($articles)-11 && $i >= 0; $i--)
+        <tr class="line">
+            <td>
+                <a href="{{ route('admin.articles.edit', $articles[$i]->id) }}" title="Modifier l'article">{{ $articles[$i]->name }}</a>
+            </td>
+            <td>
+                {{ $articles[$i]->intro }}
+            </td>
+            <td>
+                {{ $articles[$i]->category->name }}
+            </td>
+            <td>
+                @foreach($articles[$i]->users as $autor)
+                    {{ $autor->username }}
+                @endforeach
+            </td>
+            <td>
+                {{ $articles[$i]->created_at }}
+            </td>
+            <td>
+                {{ $articles[$i]->published_at }}
+            </td>
+            <td class="center aligned">
+                <div class="four wide column">
+                    <div class="flex-center">
+                        <button class="circular ui blue icon button" title="Voir l'aperçu de l'article" onclick="window.open('{{ route('article.show', $articles[$i]->article_url) }}')">
+                            <i class="icon eye"></i>
                         </button>
-                    </form>
+                        <!-- Formulaire de suppression -->
+                        <form action="{{ route('admin.articles.destroy', $articles[$i]->id) }}" method="post" >
+                            {{ csrf_field() }}
+
+                            <input type="hidden" name="_method" value="DELETE">
+
+                            <button class="circular ui red icon button" title="Supprimer l'article" value="Supprimer cet article ?" onclick="return confirm('Voulez-vous vraiment supprimer cet article ?')">
+                                <i class="icon remove"></i>
+                            </button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </td>
+            </td>
     </tr>
+    @endfor
 @endcomponent
