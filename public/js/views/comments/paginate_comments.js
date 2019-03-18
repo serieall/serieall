6ea1ed2cc6844bc981d90a52545b18ac697c5e9c
elpiolito <jojo.jumper@hotmail.fr>
2019-03-18 16:33:31 +0100

    Change - regroup js in one common file for comment pagination

---
+++ public/js/views/comments/paginate_comments.js
@@ -0,0 +1,39 @@
+
+    $(window).on('hashchange', function() {
+        if (window.location.hash) {
+            var page = window.location.hash.replace('#', '');
+            if (page == Number.NaN || page <= 0) {
+                return false;
+            } else {
+                getComments(page);
+            }
+        }
+    });
+
+    $(document).ready(function() {
+        $(document).on('click', '.pagination a', function (e) {
+            getComments($(this).attr('href').split('page=')[1]);
+            e.preventDefault();
+            $('#ListAvis').addClass('loading');
+        });
+    });
+
+    function getComments(page) {
+        $.ajax({
+            url : '?page=' + page,
+            dataType: 'json'
+        }).done(function (data) {
+            // On insére le HTML
+            $('#LastComments').html(data);
+
+            // On recharge les spoilers et on remonte en haut de la page.
+            $.getScript('/spoiler/spoiler.js');
+            $('html, body').animate({scrollTop:$('#ListAvis').offset().top}, 'slow');//return false;
+
+            location.hash = page;
+            $('#ListAvis').removeClass('loading');
+        }).fail(function () {
+            alert('Les commentaires n\'ont pas été chargés.');
+            $('#ListAvis').removeClass('loading');
+        });
+    }
