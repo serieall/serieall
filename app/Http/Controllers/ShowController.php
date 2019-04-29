99406a8aa4c822d70f226899b6d370838836a6e8
elpiolito <jojo.jumper@hotmail.fr>
2019-04-29 16:00:36 +0200

    Change - create trait to regrou treatment on show header

--- app/Http/Controllers/ShowController.php
+++ app/Http/Controllers/ShowController.php
@@ -20,6 +20,8 @@ use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\View;
 
+use App\Traits\FormatShowHeaderTrait;
+
 /**
  * Class ShowController
  * @package App\Http\Controllers
@@ -32,6 +34,8 @@ class ShowController extends Controller
     protected $commentRepository;
     protected $articleRepository;
     protected $categoryRepository;
+
+    use FormatShowHeaderTrait;
 
     /**
      * ShowController constructor.
@@ -262,52 +266,4 @@ public function index($channel = "0", $genre = "0", $nationality = "0", $tri = 1
         return view('shows.statistics', compact('showInfo', 'topEpisodes'));
     }
 
-
-    /************ Private **************/
-
-    /**
-     * Format data for displaying show header.
-     * @param $show
-     * @return array
-     */
-    private function formatForShowHeader($show){
-        $articles = [];
-
-        $nbcomments = $this->commentRepository->getCommentCountByTypeForShow($show->id);
-
-        $showPositiveComments = $nbcomments->where('thumb', '=', '1')->first();
-        $showNeutralComments = $nbcomments->where('thumb', '=', '2')->first();
-        $showNegativeComments = $nbcomments->where('thumb', '=', '3')->first();
-
-        // On récupère les saisons, genres, nationalités et chaines
-
-        $genres = formatRequestInVariable($show->genres);
-        $nationalities = formatRequestInVariable($show->nationalities);
-        $channels = formatRequestInVariable($show->channels);
-
-        // On récupère la note de la série, et on calcule la position sur le cercle
-        $noteCircle = noteToCircle($show->moyenne);
-
-        // Détection du résumé à afficher (fr ou en)
-        if(empty($show->synopsis_fr)) {
-            $synopsis = $show->synopsis;
-        }
-        else {
-            $synopsis = $show->synopsis_fr;
-        }
-
-        // Faut-il couper le résumé ? */
-        $numberCharaMaxResume = config('param.nombreCaracResume');
-        if(strlen($synopsis) <= $numberCharaMaxResume) {
-            $showSynopsis = $synopsis;
-            $fullSynopsis = false;
-        }
-        else {
-            $showSynopsis = cutResume($synopsis);
-            $fullSynopsis = true;
-        }
-
-        return compact('show', 'genres', 'nationalities', 'channels', 'noteCircle', 'synopsis', 'showSynopsis', 'fullSynopsis', 'showPositiveComments', 'showNeutralComments', 'showNegativeComments', 'articles');
-
-    }
 }
