f63b44aa0f009f683365e89694385353fab0d42c
elpiolito <jojo.jumper@hotmail.fr>
2019-06-12 14:58:05 +0200

    #188 - Espace insécable plugin

--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -267,13 +267,14 @@
     <script>
         CKEDITOR.plugins.addExternal( 'spoiler', '/js/ckeditor/plugins/spoiler/plugin.js' );
         CKEDITOR.plugins.addExternal( 'wordcount', '/js/ckeditor/plugins/wordcount/plugin.js' );
-	CKEDITOR.plugins.addExternal( 'youtube', '/js/ckeditor/plugins/youtube/plugin.js' );
+	    CKEDITOR.plugins.addExternal( 'youtube', '/js/ckeditor/plugins/youtube/plugin.js' );
+	    CKEDITOR.plugins.addExternal( 'nbsp', '/js/ckeditor/plugins/nbsp/plugin.js' );
 
         CKEDITOR.replace( 'article' ,
             {
                 toolbar: 'Basic',
                 filebrowserBrowseUrl : '/js/elFinder/elfinder_cke.html',
-                extraPlugins: 'spoiler,wordcount,youtube',
+                extraPlugins: 'spoiler,wordcount,youtube, nbsp',
                 wordcount: {
                     showCharCount: true,
                     showWordCount: false,
